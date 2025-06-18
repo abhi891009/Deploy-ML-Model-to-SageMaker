438768d4f868dcf9c67ba0675f03b61029c06e56
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-06-18 21:44:01 +0000

    Creating the ML-Model infra

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -10,6 +10,11 @@ resource "aws_sagemaker_model" "ml_model" {
   name               = "mlflow-model"
   execution_role_arn = var.sagemaker_role_arn
 
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes = all
+  }
+
   primary_container {
     image          = "921666318654.dkr.ecr.us-east-1.amazonaws.com/myapp-repo"
     model_data_url = "s3://abhi100bucket1211/upload/model.tar.gz"
